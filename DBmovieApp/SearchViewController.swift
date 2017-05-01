6ededbc001307bcc6f8a5c90fb3e1a70a2e3c530
Dmitriy <Medved@dmitriys-mac-mini.local>
2017-05-01 11:22:25 +0300

    show the search input in table view and few UI improvements

--- DBmovieApp/SearchViewController.swift
+++ DBmovieApp/SearchViewController.swift
@@ -12,6 +12,8 @@ class SearchViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+    
+    var searchResults = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,4 +29,55 @@ class SearchViewController: UIViewController {
 
 
 }
+// MARK: -> EXTENSIONS
+// MARK: -> search bar delegate
+extension SearchViewController: UISearchBarDelegate {
+    // paints the white gap above the search bar
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // hide the keybord until tap inside the search bar
+        searchBar.resignFirstResponder()
+        
+        for i in 0...2 {
+            searchResults.append(String(format: "fake result %d for '%@' ", i, searchBar.text!))
+        }
+        tableView.reloadData()
+        print("The search text is: \(searchBar.text!)")
+    }
+}
+// MARK: -> table view data source
+extension SearchViewController: UITableViewDataSource {
+    
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchResultCell"
+        var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        }
+        cell.textLabel!.text = searchResults[indexPath.row]
+        return cell
+    }
+    
+}
+// MARK: -> table view delegate
+extension SearchViewController: UITableViewDelegate {
+    
+}
+
+
+
+
+
+
+
+
+
+
 
