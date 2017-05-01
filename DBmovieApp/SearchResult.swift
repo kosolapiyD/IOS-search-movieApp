7511976ba1d465777ce72525b8f473849daf381c
Dmitriy <Medved@dmitriys-mac-mini.local>
2017-05-01 12:18:19 +0300

    add has searched or not logic and "nothing found" string

---
+++ DBmovieApp/SearchResult.swift
@@ -0,0 +1,25 @@
+//
+//  SearchResult.swift
+//  DBmovieApp
+//
+//  Created by Dmitriy on 01/05/2017.
+//  Copyright © 2017 Dmitriy. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    
+    var title = ""
+    var releaseDate = ""
+    var genre = 0
+    var rateAverage = 0.0
+    var posterPath = ""
+}
+
+
+
+
+
+
+
--- DBmovieApp/SearchViewController.swift
+++ DBmovieApp/SearchViewController.swift
@@ -13,7 +13,8 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
-    var searchResults = [String]()
+    var searchResults = [SearchResult]()
+    var hasSearched = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,33 +43,62 @@ extension SearchViewController: UISearchBarDelegate {
         searchBar.resignFirstResponder()
         
         for i in 0...2 {
-            searchResults.append(String(format: "fake result %d for '%@' ", i, searchBar.text!))
+            let searchResult = SearchResult()
+            searchResult.title = String(format: "fake result %d for ", i)
+            searchResult.releaseDate = searchBar.text!
+            searchResults.append(searchResult)
         }
+        hasSearched = true
         tableView.reloadData()
-        print("The search text is: \(searchBar.text!)")
     }
 }
 // MARK: -> table view data source
 extension SearchViewController: UITableViewDataSource {
     
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return searchResults.count
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if !hasSearched {
+            return 0
+        } else if searchResults.count == 0 {
+            return 1
+        } else {
+            return searchResults.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cellIdentifier = "SearchResultCell"
         var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
         if cell == nil {
-            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
         }
-        cell.textLabel!.text = searchResults[indexPath.row]
+        
+        if searchResults.count == 0 {
+            cell.textLabel!.text = "(Nothing Found)"
+            cell.detailTextLabel!.text = ""
+        } else {
+            let searchResult = searchResults[indexPath.row]
+            cell.textLabel!.text = searchResult.title
+            cell.detailTextLabel!.text = searchResult.releaseDate
+        }
         return cell
     }
     
 }
 // MARK: -> table view delegate
 extension SearchViewController: UITableViewDelegate {
-    
+    // deselct the row with animation, not selected all the time
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    // makes sure that can only select rows with actual search results
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if searchResults.count == 0 {
+            return nil
+        } else {
+            return indexPath
+        }
+    }
 }
 
 
